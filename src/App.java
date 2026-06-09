af0181aded6bc9dd180a41bd0eb4c472c33c775f
kaushal9576 <kaushalpatel70e@gmail.com>
2026-06-09 17:17:43 +0530

    saved

--- src/App.java
+++ src/App.java
@@ -1,5 +1,5 @@
 public class App {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         System.out.println("Hello, World!");
         System.out.println("hi git hub");
         System.out.println("hi  git hulu hulu");
