a39418bfb240928557ea37911f0dbdc4b3910bde
Kaushal Patel <139335691+Kaushal9576@users.noreply.github.com>
2026-06-09 17:12:30 +0530

    Update App.java

--- src/App.java
+++ src/App.java
@@ -2,5 +2,6 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         System.out.println("hi git hub");
+        System.out.println("hi  git hulu hulu");
     }
 }
